c7ded6b712e7e2de696f167650b7cd60ab1d6a6f
Caoimhín McDonald <caoimhinmcdonald@hotmail.com>
2026-05-01 10:53:34 +0100

    Add tests

--- tests/test_system.py
+++ tests/test_system.py
@@ -19,3 +19,15 @@ def test_title_name(driver):
     driver.get("http://127.0.0.1:5000/")
     title = driver.title
     assert title == "Grade Marking System"
+
+def test_h2(driver):
+    driver.get("http://127.0.0.1:5000/")
+    h2 = driver.find_element(By.ID, "h2")
+    assert h2.text == "Load Student File"
+
+def test_file_name(driver):
+    driver.get("http://127.0.0.1:5000/")
+    filename = driver.find_element(By.ID, "filename")
+    filename.send_keys("students.txt")
+    button = driver.find_element(By.ID, "submit-btn")
+    button.click()
