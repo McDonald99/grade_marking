f227be762cf48830c5dac7729b9175afb1aea2c6
Caoimhín McDonald <caoimhinmcdonald@hotmail.com>
2026-05-04 17:51:07 +0100

    test table row

--- tests/test_system.py
+++ tests/test_system.py
@@ -7,6 +7,7 @@ from selenium.webdriver.chrome.service import Service as ChromeService
 
 
 from selenium.webdriver.support.select import Select
+from selenium.webdriver.support.color import Color
 
 
 @pytest.fixture
@@ -14,11 +15,15 @@ def driver():
     driver = webdriver.Chrome(service=ChromeService('./tests/chromedriver'))
     return driver
 
-
 def test_title_name(driver):
     driver.get("http://127.0.0.1:5000/")
     title = driver.title
     assert title == "Grade Marking System"
+
+def test_h1(driver):
+    driver.get("http://127.0.0.1:5000/")
+    h1 = driver.find_element(By.ID, "h1")
+    assert h1.text == "Grade Marking System"
 
 def test_h2(driver):
     driver.get("http://127.0.0.1:5000/")
@@ -31,3 +36,20 @@ def test_file_name(driver):
     filename.send_keys("students.txt")
     button = driver.find_element(By.ID, "submit-btn")
     button.click()
+
+def test_table_r1(driver):
+    driver.get("http://127.0.0.1:5000/")
+    filename = driver.find_element(By.ID, "filename")
+    filename.send_keys("students.txt")
+    button = driver.find_element(By.ID, "submit-btn")
+    button.click()
+
+    fname = driver.find_element(By.XPATH,"//table/tbody/tr[1]/td[1]").text
+    score = driver.find_element(By.XPATH,"//table/tbody/tr[1]/td[2]").text
+    grade = driver.find_element(By.XPATH,"//table/tbody/tr[1]/td[3]").text
+    bgcolour = Color.from_string(driver.find_element(By.CLASS_NAME,"grade-1").value_of_css_property("background-color"))
+
+    assert fname == "Evan"
+    assert score == "99.0"
+    assert grade == "1"
+    assert bgcolour.rgb == "rgb(255, 215, 0)"
